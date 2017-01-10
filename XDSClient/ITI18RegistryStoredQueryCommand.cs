ddd19a08a051e3dc0839ca17a8751de20b06bc18
Bernt Brenna <bernt.brenna@gmail.com>
2017-01-10 15:47:03 +0100

    Added XDS client test executable

---
+++ XDSClient/ITI18RegistryStoredQueryCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using XDSDotNet;
+
+using static System.Console;
+
+namespace XDSClient
+{
+    public class ITI18RegistryStoredQueryCommand
+    {
+        private const string ENDPOINTNAME = "ITI18";
+        private static readonly MessageVersion MESSAGEVERSION = MessageVersion.Soap12WSAddressing10;
+
+        static public int ExecuteFromFile(string requestFilename) => ExecuteUsingRequestBody(XElement.Load(requestFilename));
+
+        static public int ExecuteUsingPatientId(string patientId) => ExecuteUsingRequestBody(Requests.FindDocuments_ITI18(patientId));
+
+        static public int ExecuteUsingRequestBody(XElement requestBody)
+        {
+            var f = new ChannelFactory<IRegistryStoredQueryITI18>(ENDPOINTNAME);
+            var channel = f.CreateChannel();
+            var request = Message.CreateMessage(
+                MESSAGEVERSION,
+                "urn:ihe:iti:2007:RegistryStoredQuery",
+                requestBody
+            );
+            var response = channel.RegistryStoredQuery(request);
+            WriteLine(response);
+            return 0;
+        }
+    }
+}
---
+++ XDSClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XDSDotNet;
+using Microsoft.Extensions.CommandLineUtils;
+using static System.Console;
+
+namespace XDSClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.HelpOption("-?|-h|--help");
+            app.Command("iti18", command =>
+            {
+                command.Description = "Execute ITI18 RegistryStoredQuery by providing the request SOAP body";
+                command.HelpOption("-?|-h|--help");
+                var argument = command.Argument("requestfilename", "Request file name");
+                command.OnExecute(() => {
+                    if (argument.Value == null)
+                    {
+                        return ErrorAndRetval($"Argument {argument.Name} not specified");
+                    }
+                    return ITI18RegistryStoredQueryCommand.ExecuteFromFile(argument.Value);
+                });
+            });
+
+            app.Command("finddocuments", command => {
+                command.Description = "Execute ITI18 RegistryStoredQuery by providing a patient ID";
+                command.HelpOption("-?|-h|--help");
+                var argument = command.Argument("patientid", "Patient ID");
+                command.OnExecute(() => {
+                    if (argument.Value == null)
+                    {
+                        return ErrorAndRetval($"Argument {argument.Name} not specified");
+                    }
+                    return ITI18RegistryStoredQueryCommand.ExecuteUsingPatientId(argument.Value);
+                });
+            });
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 2;
+            });
+
+            app.Execute(args);
+
+        }
+
+        static public int ErrorAndRetval(string msg)
+        {
+            Error.WriteLine($"ERROR: {msg}");
+            return 2;
+        }
+    }
+}
