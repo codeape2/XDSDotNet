9624fc632a2c4de3db59fad7497a4f0052c854df
Bernt R. Brenna <bernt.roskar.brenna@ehelse.no>
2017-01-10 12:59:44 +0000

    Added servicecontract interfaces for ITI38 and ITI39

---
+++ XDSDotNet/ICrossGatewayQueryITI38.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace XDSDotNet
+{
+    [ServiceContract(Namespace = "urn:ihe:iti:xds-b:2007")]
+    public interface ICrossGatewayQueryITI38
+    {
+        [OperationContract(Action = "urn:ihe:iti:2007:CrossGatewayQuery", ReplyAction = "urn:ihe:iti:2007:CrossGatewayQueryResponse")]
+        Message CrossGatewayQuery(Message request);
+
+    }
+}
---
+++ XDSDotNet/ICrossGatewayRetrieveITI39.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace XDSDotNet
+{
+    [ServiceContract(Namespace = "urn:ihe:iti:xds-b:2007")]
+    public interface ICrossGatewayRetrieveITI39
+    {
+        [OperationContract(Action = "urn:ihe:iti:2007:CrossGatewayRetrieve", ReplyAction = "urn:ihe:iti:2007:CrossGatewayRetrieveResponse")]
+        Message CrossGatewayQuery(Message request);
+    }
+}
